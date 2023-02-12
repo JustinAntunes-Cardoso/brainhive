91fbd7711ce6c0d9ea7c31f4b0b8fc6e4392800f
Justin Antunes-Cardoso <justin.antunes-cardoso@hotmail.com>
2023-02-12 14:12:57 -0500

    Home page styling and functionality is completed. Buttons choose which axios call is made.

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import Hero from '../../components/Hero'
-import './Home.scss'
+// import { useState } from "react";
+// import Login from "../Login";
+// import Signup from "../Signup"
+// import Profile from "../Profile";
+import Hero from '../../components/Hero';
+
+import './Home.scss';
 
 function HomePage() {
+  // const [isUserLoggedIn, setIsUserLoggedIn] = useState(!!sessionStorage.bearerToken);
+  // const [mode,setMode] = useState("signup"); 
+
+  // function onLogin(token) {
+  //   sessionStorage.setItem("bearerToken", token);
+  //   setIsUserLoggedIn(true);
+  // }
+
+  // function onLogout() {
+  //   sessionStorage.removeItem("bearerToken");
+  //   setIsUserLoggedIn(false);
+  // }
+
   return (
-    <main className='home'>
-      <Hero />
-    </main>
-  )
+		<main className='home'>
+      {/* <img className="home__image" src={honey} alt="Honey Drip" /> */}
+      {/* {isUserLoggedIn ? <Profile onLogout={onLogout} /> : mode==="login" ? <Login onLogin={onLogin} setMode={setMode}/> : <Signup onLogin={onLogin} setMode={setMode}/>} */}
+			<Hero />
+		</main>
+	);
 }
 
-export default HomePage
+export default HomePage;
